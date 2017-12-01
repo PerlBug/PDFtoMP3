73a9b0903ebfba4329c25571215db3f10993e9f4
Mko Galoyan <z5157086@student.unsw.edu.au>
2017-12-02 00:01:52 +1100

    added gui

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-from Tkinter import *
+#main project file. contains most of the GUI code.
+
+from tkinter import *
+
+root = Tk()
+root.geometry("250x150")
+
+
+topFrame = Frame(root)
+topFrame.pack()
+
+label_1 = Label(topFrame, text="PDF Directory")
+entry_1 = Entry(topFrame)
+
+label_1.grid(row=0, column=0)
+entry_1.grid(row=0, column=1)
+
+bottomFrame = Frame(root)
+bottomFrame.pack(side=BOTTOM)
+
+exitButton = Button(bottomFrame, text="Quit", command=root.destroy)
+exitButton.pack()
+
+
+
+
+root.mainloop()
